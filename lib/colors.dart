4827bf0db2410beee80cf420eff149c83fa5d6bd
Ercan Tomac <48109156+ercantomac@users.noreply.github.com>
2021-12-16 16:11:02 +0100

    Update colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,10 +3,4 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const Color accentColor = Color(0xFFFF5151);
   static const Color darkColor = Color(0xFF121212);
-  /*static const Color accentColor2 = Color(0xFF2979FF);
-  static const Color accentColor3 = Color(0xFF1DE9B6);
-  static const Color accentColor4 = Color(0xFFFF9100);
-  static const Color accentColor5 = Color(0xFF8E24AA);
-  static const Color accentColor6 = Color(0xFF64DD17);
-  static const Color accentColor7 = Color(0xFFD50000);*/
 }
